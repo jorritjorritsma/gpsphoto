53ef460845160f8baf1c0bd7ee386bf88869c0b9
Jorrit Jorritsma <jsj@xs4all.nl>
2017-08-06 23:56:36 +0200

    Forgot this file....

--- wsgi/gpsphoto/gpsphoto.py
+++ wsgi/gpsphoto/gpsphoto.py
@@ -164,13 +164,19 @@ class GpsPhoto:
             except:
                 bearing = None
 
-            self.coordinates = {'mapdatum': gps_mapdatum, 'lon': lon, 'lat': lat, 'bearing': bearing, 'z': z}
+            self.coordinates = {'mapdatum': gps_mapdatum.rstrip(), 'lon': lon, 'lat': lat, 'bearing': bearing, 'z': z}
             return(True)
         except Exception, e:
-            print 'We bailed out of getCoordinates'
-            print str(e)
+            exc_obj = sys.exc_info()[1]
+            exc_tb = sys.exc_info()[2]
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(str(e))
+            print("%s line %s\n" % (str(fname), str(exc_tb.tb_lineno)))
+            print('Failed on getCoordinates')
             self.coordinates = {}
             return(None)
+    
+    #def setCoordinates(self):
         
     def getDate(self):
         try:
@@ -237,16 +243,24 @@ class GpsPhoto:
             self.epoch = None
             return(None)
         
-    #def getResizedImage(self, width=1280, height=1024, thumbwidth=200, thumbheight=133):
-    def getResizedImage(self, width=self.config.imgWidth, height=self.config.imgHeight, thumbwidth=self.config.thumbWidth, thumbheight=self.config.thumbHeight):
+    def getResizedImage(self, imgWidth=None, imgHeight=None, thumbWidth=None, thumbHeight=None):
+        if imgWidth is None:
+            imgWidth = self.config.IMG_WIDTH
+        if imgHeight is None:
+            imgHeight = self.config.IMG_HEIGHT
+        if thumbWidth is None:
+            thumbWidth = self.config.THUMB_WIDTH
+        if thumbHeight is None:
+            thumbHeight = self.config.THUMB_HEIGHT
+
         try:
-            oldsize = self.image.size
-            ratio = min(float(width)/oldsize[0], float(height)/oldsize[1])
-            size = int(oldsize[0] * ratio), int(oldsize[1] * ratio)
+            oldSize = self.image.size
+            ratio = min(float(imgWidth)/oldSize[0], float(imgHeight)/oldSize[1])
+            size = int(oldSize[0] * ratio), int(oldSize[1] * ratio)
             self.resizedImage = self.image.resize((size), Image.ANTIALIAS)
 
-            ratio = min(float(thumbwidth)/oldsize[0], float(thumbheight)/oldsize[1])
-            size = int(oldsize[0] * ratio), int(oldsize[1] * ratio)
+            ratio = min(float(thumbWidth)/oldSize[0], float(thumbHeight)/oldSize[1])
+            size = int(oldSize[0] * ratio), int(oldSize[1] * ratio)
             self.thumbnail = self.image.resize((size), Image.ANTIALIAS)
 
         except Exception, e:
@@ -282,7 +296,7 @@ class GpsDb:
         #               'description': <description>,
         #               'url': <photo url>,
         #               'thumburl: <url of thumbnail>,
-        #               'type': <cartographic type>,
+        #               'incidenttype': <cartographic type>,
         #               'userid': <email address / user identifier>,
         #               'event': <event name>,
         #               'positioningmethod': <'GPS'|'manual'>
@@ -290,7 +304,8 @@ class GpsDb:
         #               'phototime': <yyyy-mm-dd hh:mm:ss UTC>}
         # }
         try:
-            (sql, data) = _updateOrModifySql(self, rowDict, 'insert')
+            (sql, data) = self._updateOrModifySql(rowDict, 'insert')
+
             self.cur.execute(sql, data)
             self.cur.execute('SELECT LASTVAL()')
             lastid = self.cur.fetchone()[0]
@@ -310,7 +325,7 @@ class GpsDb:
         # 'values' : {  'guid': <guid>,
         #               'title': <title>,
         #               'description': <description>,
-        #               'type': <cartographic type>,
+        #               'incidenttype': <cartographic type>,
         #               'event': <name of event>,
         #               'verified': <Boolean>,
         #               'positioningmethod': <'GPS'|'manual'>
@@ -320,15 +335,15 @@ class GpsDb:
             print("Trying to update a record without specifying a guid")
             return(None)
         try:
-            (sql, data) = _updateOrModifySql(self, rowDict, 'update')
-            self.cur.execute(sql, data)
+            (sql, data) = self._updateOrModifySql(rowDict, 'update')
+            self.cur.execute(sql, data, )
             self.conn.commit()
             return(True)
         except Exception, e:
             print(str(e))
             raise Exception("Failed to update record")
 
-    def _updateOrModifySql(rowDict, type):
+    def _updateOrModifySql(self, rowDict, type):
         # rowdict = {
         # 'coordinates' : {     'lat' : <latitude>,
         #                       'lon' : <longitude>,
@@ -341,7 +356,7 @@ class GpsDb:
         #               'description': <description>,
         #               'url': <photo url>,
         #               'thumburl: <url of thumbnail>,
-        #               'type': <cartographic type>,
+        #               'incidenttype': <cartographic type>,
         #               'userid': <email address / user identifier>,
         #               'event': <event name>,
         #               'positioningmethod': <'GPS'|'manual'>
@@ -350,61 +365,61 @@ class GpsDb:
         # },
         # type = 'insert'|'update'
         
-        values = rowDict['values']
-        
-        # let's process the coordinates
-        lat = rowDict['coordinates']['lat']
-        lon = rowDict['coordinates']['lon']
-
-        bearing = rowDict['coordinates']['bearing']
-        if 'z' not in rowDict['coordinates'] or not rowDict['coordinates']['z']:
-            z = -99999
-        else:
-            z = rowDict['coordinates']['z']
-            
-        mapdatum = rowDict['coordinates']['mapdatum']
-        if mapdatum == 'WGS-84':
-            rsid = 4326
-        else:
-            rsid = 4326 # don't know how to handle this yet....
-        
         fields = {}
         # all normal attributes get the same sql statent format
-        for item in values:
-            fields[item] = '%s'
+        for item in rowDict['values']:
+            fields[item] = '%s' # fields for psychopg2 sql statement
         
-        # geom is special
-        values['geom'] = [lon, lat, z, rsid]
-        fields['geom'] = 'ST_SetSRID(ST_MakePoint(%s, %s, %s), %s)'
+        # if this is an update, coordinates don't have to be provided.
+        # if they are however, they need to be complete
+        if 'coordinates' in rowDict:
+            # let's process the coordinates
+            lat = rowDict['coordinates']['lat']
+            lon = rowDict['coordinates']['lon']
+
+            bearing = rowDict['coordinates']['bearing']
+            if 'z' not in rowDict['coordinates'] or not rowDict['coordinates']['z']:
+                z = -99999
+            else:
+                z = rowDict['coordinates']['z']
+            
+            mapdatum = rowDict['coordinates']['mapdatum']
+            if mapdatum == 'WGS-84':
+                rsid = 4326
+            else:
+                rsid = 4326 # don't know how to handle this yet....
+           
+            # geom is special
+            rowDict['values']['geom'] = [lon, lat, z, rsid]
+            fields['geom'] = 'ST_SetSRID(ST_MakePoint(%s, %s, %s), %s)'
         
-        # bearing was considered part of the geometry, hence added here
-        values['bearing'] = coordinates['bearing']
-        fields['bearing'] = '%s'
+            # bearing was considered part of the geometry, hence added here as normal attribute
+            rowDict['values']['bearing'] = rowDict['coordinates']['bearing']
+            fields['bearing'] = '%s'
         
-        columns = []
-        columnValues = []
-        data = []
+        columns = [] # columns to build sql statement
+        values = [] # fields to build sql statement
+        data = [] # data (tuple) to use with sql execute
         for item in fields:
             columns.append(item)
-            columnValues.append(fields[item])
+            values.append(fields[item])
             if item == 'geom':
-                data += values['geom']
-        else:
-            data.append(values[item])
+                data += rowDict['values']['geom']
+            else:
+                data.append(rowDict['values'][item])
             
         if type == 'insert':
-            sql = "INSERT INTO %s (%s) VALUES (%s)" % (self.gpsPhotoTable, ','.join(columns), ','.join(columnValues))
+            sql = "INSERT INTO %s (%s) VALUES (%s)" % (self.gpsPhotoTable, ','.join(columns), ','.join(values))
         elif type == 'update':
             # We'll be overwriting the guid with itself, but that should be ok
-            sql = "UPDATE %s SET (%s) = (%s) WHERE guid = %s" % (self.gpsPhotoTable, ','.join(columns), ','.join(columnValues), values['guid'])
-            data.append(values['guid'])
+            sql = "UPDATE {} SET ({}) = ({}) WHERE guid = %s".format(self.gpsPhotoTable, ','.join(columns), ','.join(values))
+            data.append(rowDict['values']['guid'])
         else:
             return(None)
         
         # let's for now see what sql we are getting
         print(sql)
         print(str(tuple(data)))
-        
         return(sql, tuple(data))
 
     def getPhotoPoints(self, columns=[], orderColumn='phototime', order='DESC', query=None, data=None, limit='ALL'):
@@ -440,16 +455,39 @@ class GpsDb:
             try:
                 pythonicResult.append([ast.literal_eval(result[i][0])] + list(result[i][1:]))
             except Exception, e:
-               print(str(e))
-               print(result[i][0])
-               print(result[i][1:])
-               return(None)
+                exc_obj = sys.exc_info()[1]
+                exc_tb = sys.exc_info()[2]
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print(str(e))
+                print(result[i][0])
+                print(result[i][1:])
+                print("%s line %s\n" % (str(fname), str(exc_tb.tb_lineno)))
+                raise Exception("Failed to do getPhotoPoints")
         return(pythonicResult)
     
+    def getGpsPhotoRow(self, guid, fields):
+        try:
+            sql = "select {} from {} where guid = %s".format(",".join(fields), self.gpsPhotoTable)
+            self.cur.execute(sql, [guid])
+            result = self.cur.fetchone()
+            return(result)
+        except Exception, e:
+            print(str(e))
+            raise Exception("Failed to do getGpsPhotoRow")
+
+    def verifyPhoto(self, guid, status):
+        try:
+            sql = "update {} SET verified = %s where guid = %s".format(self.gpsPhotoTable)
+            self.cur.execute(sql, [status, guid])
+            self.conn.commit()
+        except Exception, e:
+            print(str(e))
+            raise Exception("Could not verify / unverify record")
+
     def deleteGpsPhotoRow(self, guid):
         try:
             sql = "delete from {} where guid = %s".format(self.gpsPhotoTable)
-            self.cur.execute(sql, (guid))
+            self.cur.execute(sql, [guid])
             self.conn.commit()
         except Exception, e:
             print(str(e))
@@ -473,12 +511,29 @@ class PhotoStore:
         try:
             self.bucket = self.connS3.get_bucket(self.config.BUCKET)
         except:
-            self.bucket = self.connS3.create_bucket(self.config.BUCKET)
+            try:
+                self.bucket = self.connS3.create_bucket(self.config.BUCKET)
+            except Exception, e:
+                exc_obj = sys.exc_info()[1]
+                exc_tb = sys.exc_info()[2]
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print str(e)
+                print("%s line %s\n" % (str(fname), str(exc_tb.tb_lineno)))
+                raise Exception("Could not open or create S3 bucket")
+
         
     def deleteFile(self, fileName):
-        k = Key(self.bucket)
-        k.key = fileName
-        self.bucket.delete_key(k)
+        try:
+            k = Key(self.bucket)
+            k.key = fileName
+            self.bucket.delete_key(k)
+        except Exception, e:
+            exc_obj = sys.exc_info()[1]
+            exc_tb = sys.exc_info()[2]
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print str(e)
+            print("%s line %s\n" % (str(fname), str(exc_tb.tb_lineno)))
+            raise Exception("failed to delete file from S3")
     
     def storeImage(self, image=None, fileName=None, imgFormat=None, exif=None):
         # image is a PIL Image object, fileName the full name including path on how it should be stored
@@ -497,8 +552,11 @@ class PhotoStore:
                 k.set_contents_from_string(fileContents)
                 k.make_public()
             except Exception, e:
-                print "failed storeImage"
+                exc_obj = sys.exc_info()[1]
+                exc_tb = sys.exc_info()[2]
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
                 print str(e)
-                return(False)
+                print("%s line %s\n" % (str(fname), str(exc_tb.tb_lineno)))
+                raise Exception("failed storeImage")
         else:
-            return(False)
+            raise Exception("No suitable image provided")
