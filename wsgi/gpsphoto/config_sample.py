27663a01776ded9a1b1f731736400221101c80e9
Jorrit Jorritsma <jsj@xs4all.nl>
2017-09-09 00:14:06 +0200

    sample config in line wiht reality again

--- wsgi/gpsphoto/config_sample.py
+++ wsgi/gpsphoto/config_sample.py
@@ -1,15 +1,17 @@
 ID = ''                             # S3 ID
 KEY = ''                            # S3 key
-BUCKET = ''                         # S3 bucket (needs to be lowercase)
-S3URL = 'https://s3-eu-west-1.amazonaws.com/%s/' % BUCKET
+# S3 bucket (needs to be lowercase)
+BUCKET = {'default': 'gpsphoto', 'org1': 'gpsphoto_org1', 'org2': 'gpsphoto_org2'}
+S3URL = 'https://s3-eu-west-1.amazonaws.com'
 DB_HOST = 'localhost'               # can also be fqdn
 DB_NAME = ''                        # DB Name (postgresql)
 DB_USER = ''                        # DB User name
 DB_PASSWD = ''                      # DB Password
 DB_PHOTOTABLE = ''                  # table name
 DB_RECORDLIMIT = 2000               # nr of records to return 'ALL' returns all records
-KEEP_EXIF = {'default':True|False,
-            'some_org':True|False}  # keep exif header in saved photos
+KEEP_EXIF = {'default':False,
+            'org1':True,
+            'org2':True}            # keep exif header in saved photos
 IMG_WIDTH = 1280                    # max width of stored image
 IMG_HEIGHT = 1024                   # max height of stored image
 THUMB_WIDTH = 200                   # max widht of stored thumbnail
