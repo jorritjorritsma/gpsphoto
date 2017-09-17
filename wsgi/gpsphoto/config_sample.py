49813876da812565579b0f03bb3e3aa0d10e311c
Jorrit Jorritsma <jsj@xs4all.nl>
2017-09-17 21:47:34 +0200

    updated example config file to have separate buckets per org

--- wsgi/gpsphoto/config_sample.py
+++ wsgi/gpsphoto/config_sample.py
@@ -5,8 +5,11 @@ KEY = ''                            # S3 key
 # Bucket names should not end with a dash
 # Bucket names should be between 3 and 63 characters long
 # Bucket names cannot contain dashes next to periods (e.g., "my-.bucket.com" and "my.-bucket" are invalid)
-# Bucket names cannot contain periods - Due to our S3 client utilizing SSL/HTTPS, Amazon documentation indicates that a bucket name cannot contain a period, otherwise you will not be able to upload files from our S3 browser in the dashboard. More info here
-BUCKET = ''                         # S3 bucket (needs to be lowercase)
+# Bucket names cannot contain periods - Due to our S3 client utilizing SSL/HTTPS, Amazon documentation
+# indicates that a bucket name cannot contain a period, otherwise you will not be able to upload files
+# from our S3 browser in the dashboard
+BUCKET = {'default': '<bucket name',
+        'some_org': <some org bucket name>}
 S3URL = 'https://s3-eu-west-1.amazonaws.com/%s/' % BUCKET
 S3LOCATION = ''                     # AWS region http://docs.aws.amazon.com/general/latest/gr/rande.html
 DB_HOST = 'localhost'               # can also be fqdn
